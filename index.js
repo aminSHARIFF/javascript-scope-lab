021feeb66c87e95e437ea635a6479f269073fc5e
aminSHARIFF <amin29536@gmail.com>
2025-11-27 22:38:20 +0300

    javascript_scope_lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+// Global Scope Variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function Scope
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Block Scope
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Update Featured Drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
